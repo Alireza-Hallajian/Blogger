f16b46a77ee234858c1fc7533c8c1c35c079a64b
Alireza Hallajian <alireza.hn78@gmail.com>
2020-10-30 22:46:49 +0330

    Comment Schema

---
+++ models/comment.js
@@ -0,0 +1,35 @@
+//node_modules
+const mongoose = require('mongoose');
+
+//mongoose schema
+const Schema = mongoose.Schema;
+
+
+//******************************************************************************** */
+//                                   Comment Schema
+//******************************************************************************** */
+
+const CommentShema = new Schema(
+{
+    article_id: {
+        type: Schema.Types.ObjectId,
+        ref: "Article"
+    },
+
+    author_id: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+
+    content: {
+        type: String,
+        required: true,
+        maxlength: 1000
+    }
+});
+
+
+
+
+
+module.exports = mongoose.model('Comment', CommentShema);
